71a2ef918df1b25f6cf58886b15eb6254ad30e6c
Marcus <marcusd@q2chat.co.uk>
2022-10-07 16:27:59 +0100

    Add project files.

---
+++ EXE_Proxy/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+
+const char q = '"';
+
+string? exe = Environment.ProcessPath; // get the path+name of the exe
+if (exe != null) Go(exe, args); // make sure we got it, if so continue with the Go() method
+
+static void Go(string exe, string[] args)
+{
+    if (exe.ToLower().EndsWith(".exe")) exe = exe[..^4]; // remove .exe from end of executable
+    string arg = string.Empty; // init the args rebuild string
+    foreach (string a in args) // loop through all args
+        arg += QuoteString(a) + " "; // add quotes if the arg has a space in it and append it to our rebuild string with a space to seperate each arg
+    arg = arg.Trim(); // remove the extra space from the end
+    string orig = exe + "_orig.exe"; // init the string for the original exe
+    string[] log = { QuoteString(orig) + " " + arg + "\r\n\r\n" }; // construct the debug command line log quoting the exe if needed. Using an array to maybe add verbosity options in the future?
+    using Process process = new(); // initialize a new process
+    process.StartInfo.FileName = orig; // set the original exe to run
+    process.StartInfo.Arguments = arg; // pass in the rebuilt argument string
+    process.StartInfo.RedirectStandardOutput = true; // redirect console output streams to save in a log file
+    process.StartInfo.RedirectStandardError = true;
+    process.StartInfo.WorkingDirectory = Environment.CurrentDirectory; // set the same working dir as this app is working from
+    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden; // don't show a window
+    try
+    {
+        process.Start(); // start the process
+    }
+    catch (Exception e) // catch any errors (yes I know, sue me)
+    {
+        lock(log)
+            log[0] += "An error occured when trying to start the original exe \r\n\r\n" + e.ToString(); // add the error report to the log
+    }
+    if (!process.HasExited) // if the process is running
+    {
+        Log(process.StandardOutput, "OUTPUT: ", log); // start asynchronous tasks to capture console output
+        Log(process.StandardError,  " ERROR: ", log);
+    }
+    try { process.WaitForExit(); } catch { } // wait for process to end (if it even started)
+    WriteLogFile(exe + "_log.txt", log); // rite out the log file
+}
+
+static void WriteLogFile(string fileName, string[] log)
+{
+    try
+    {
+        lock (log)
+            File.WriteAllText(fileName, log[0]); // write text out to a file
+    }
+    catch { }
+}
+
+static async void Log(StreamReader stream, string prefix, string[] log)
+{
+    try
+    {
+        while (true) // loop forever(ish)
+        {
+            string? line = await stream.ReadLineAsync(); // wait for the next line and release execution back to the Go() method
+            if (line == null) break; // end the loop if there's an error
+            lock (log)
+                log[0] += prefix + line + "\r\n"; // add the prefix and line to the log buffer
+        }
+    }
+    catch { }
+}
+
+static string QuoteString(string s) => s.IndexOf(" ") > -1 ? q + s + q : s; // adds quotes to a string if the string contains a space
---
+++ readme.txt
@@ -0,0 +1,10 @@
+This project is a tool to aid in debugging GUI front-ends for console applications. The compiled .exe from this project replaces the console level exe being called by the GUI front-end and the original exe is renamed appending _orig to the filename (without extension)
+For example, let's say your console application exe being called from the GUI is named: qemu-system-x86_64.exe
+
+copy EXE_Proxy.exe to the folder containing qemu-system-x86_64.exe
+rename qemu-system-x86_64.exe to qemu-system-x86_64_orig.exe
+rename EXE_Proxy.exe to qemu-system-x86_64.exe
+
+When the exe is called, it will call the original executable and record a _log.txt (qemu-system-x86_64_log.txt) file containing the full command-line used to run it and all console output that was generated allowing you to see what the GUI is doing, what parameters it's passing and review the console application's output for errors etc.
+
+I wrote this primarily for debugging problems with the qtemu GUI front-end for qemu as it doesn't correctly log stuff.
